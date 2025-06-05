2410c9edb1a30d0db8f9bf2e2876ca92a92b2984
Rodrigo Andrade <rodrigo.andrade@hotmail.com>
2025-06-05 07:34:00 -0500

    Se creo el modelo chistes.cs, se arrastro la pagina mainpage a la carpeta vistas y se creo 2 vistas: AboutPage y ChistesPage, luego se creo el repositorio ChistesRepository para la api

---
+++ MauiAppChistes/Views/AboutPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace MauiAppChistes.Views;
+
+public partial class AboutPage : ContentPage
+{
+	public AboutPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ MauiAppChistes/Views/ChistesPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace MauiAppChistes.Views;
+
+public partial class ChistesPage : ContentPage
+{
+	public ChistesPage()
+	{
+		InitializeComponent();
+	}
+}
